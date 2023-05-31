8ef50677ac1440fbd215d035990e1baf909954bf
Yaroslav Timoshenko <jarzykk@gmail.com>
2023-05-31 22:05:08 +0300

    Add StateMachine

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    [SerializeField] private State _firstState;
+    [SerializeField] private float _delayBeforeStartFirstState = 0.3f;
+
+    private State _current;
+
+    public State CurrentState => _current;
+
+    private void OnEnable()
+    {
+        StartCoroutine(StartMachineAfterDelay());
+    }
+
+    private void Update()
+    {
+        if (_current == null)
+            return;
+
+        var nextState = _current.GetNextState();
+        if (nextState != null)
+            Transit(nextState);
+    }
+
+    private void Reset(State starttState)
+    {
+        _current = starttState;
+        _current?.Enter();
+    }
+
+    public void DisableStateMachine()
+    {
+        _current.Exit();
+        this.enabled = false;
+    }
+
+    private void Transit(State nextState)
+    {
+        _current?.Exit();
+        _current = nextState;
+        _current?.Enter();
+    }
+
+    private IEnumerator StartMachineAfterDelay()
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime <= _delayBeforeStartFirstState)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Reset(_firstState);
+    }
+}
---
+++ Assets/Scripts/StateMachine/Transition.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    [SerializeField] private State _targetState;
+
+    public State TargetState => _targetState;
+
+    public bool NeedTransit { get; protected set; }
+
+    private void OnEnable()
+    {
+        Enabled();
+    }
+
+    protected virtual void Enabled()
+    {
+        NeedTransit = false;
+    }
+}
